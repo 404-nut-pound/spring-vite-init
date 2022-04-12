7acac38a649e2137306c4c5934d589a2bdf7e6cc
Skynet <souls4523@gmail.com>
2022-04-13 01:14:45 +0900

    Spring Boot + Gradle + Vue + Vite로 개발 환경 구성

---
+++ src/main/java/io/hskim/springvueinit/SpringVueInitApplication.java
@@ -0,0 +1,13 @@
+package io.hskim.springvueinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVueInitApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringVueInitApplication.class, args);
+  }
+
+}
---
+++ src/test/java/io/hskim/springvueinit/SpringVueInitApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hskim.springvueinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVueInitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
