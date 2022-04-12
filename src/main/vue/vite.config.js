cf5ce624131f6b4e8d3f832ced73233fc47599cf
Skynet <souls4523@gmail.com>
2022-04-13 01:43:12 +0900

    npm 서버 관련 값 복원

--- src/main/vue/vite.config.js
+++ src/main/vue/vite.config.js
@@ -4,8 +4,14 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    // node 서버 설정
+    port: 8081,
+    proxy: 'http://localhost:8080',
+    disableHostCheck: true,
+  },
   build: {
-    emptyOutDir: true,
-    outDir: '../resources/static/',
+    emptyOutDir: true, // build 실행 시 outDir 내용을 삭제
+    outDir: '../resources/static/', // build 실행 시 결과물을 이동 시킬 디렉토리, 기본 값 'dist'
   },
 });
